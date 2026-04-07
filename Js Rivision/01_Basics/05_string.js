91ef7bf4d014175eac041340d18f84ab0a99db98
Roshan polai <roshanpolai3@gmail.com>
2026-04-07 19:55:12 +0530

    Add JavaScript basics files covering variables, data types, conversion operations, and HTML structure

---
+++ Js Rivision/01_Basics/05_string.js
@@ -0,0 +1,48 @@
+//Concartination
+// const name  = "Roshan";
+// console.log(name);
+// const SIC = "23BECE33";
+
+
+//String interpolation
+// console.log(`Student name is ${name} | SIC: ${SIC} `)
+
+// const newName = new String('Subham');
+// console.log(newName);
+// console.log(newName.charAt(3));
+// console.log(newName.indexOf('h'));
+
+// const game = "BGMI_FreeFire";
+// const newGame = game.substring(0,4); // BGMI
+// const game1= game.slice(5,14); // FreeFire
+
+// Substring | Slice
+
+// slice(start, end)
+// Supports negative indices
+// If start > end → returns empty string
+
+// substring(start, end)
+// Does not support negative indices (treated as 0)
+// If start > end → swaps values
+
+// let str = "JavaScript";
+
+// console.log(str.slice(0, 4));      // "Java"
+// console.log(str.substring(0, 4));  // "Java"
+
+// console.log(str.slice(-6));      // "Script"
+// console.log(str.substring(-6));  // "JavaScript"
+
+//trim()
+// const email ="roshan@gmail.com    ";
+// console.log(email);
+// console.log(email.trim()); 
+// //replace()
+// console.log(email.replace('roshan', 'subham'))
+
+// const url = "https://roshan.profile.com";
+// console.log(url.includes('https')); // true
+// console.log(url.split('.')); //[ 'https://roshan', 'profile', 'com' ]
+
+
